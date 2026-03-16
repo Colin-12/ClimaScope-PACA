e732060fc27ff9ad1574e55a04f644d496f22919
ARMAND COLIN KOMTCHEU VEIGNE LEUKAM <75719793+Colin-12@users.noreply.github.com>
2026-03-16 20:07:36 +0100

    Update inspect_meteo.py

--- src/data/inspect_meteo.py
+++ src/data/inspect_meteo.py
@@ -1,4 +1,3 @@
-cat > src/data/inspect_meteo.py <<'PY'
 from pathlib import Path
 
 import pandas as pd
@@ -58,4 +57,3 @@ if __name__ == "__main__":
 
     for file_path in files:
         inspect_file(file_path)
-PY
